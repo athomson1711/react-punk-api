7e763d144a0d32cfc3698e2bbaebf1cdb0408e34
Andrew Thomson <athomson1711@gmail.com>
2022-07-21 20:31:07 +0100

    altered cards styling

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -3,15 +3,15 @@ import Card from '../Card/Card'
 import styles from './CardList.module.scss'
 
 const CardList = (props) => {
-const { beers } = props
+  const { beers } = props;
 
   const getBeerCardsJsx = (beer) => {
     <div className={styles.card} key={beer.id}>
-        <Card beer={beer} />
+      <Card beer={beer} />
     </div>
-}
+  }
 
-  return <section className={styles.cards}>{beers.map(getBeerCardsJsx)}</section>
+  return <section className={styles.cards}> {beers.map(getBeerCardsJsx)} </section>
 }
 
 export default CardList
