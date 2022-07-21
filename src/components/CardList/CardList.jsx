0b67e6703631e6916528996cc98ed7965668ff12
Andrew Thomson <athomson1711@gmail.com>
2022-07-21 20:39:52 +0100

    API cards now displyaing

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,11 +5,11 @@ import styles from './CardList.module.scss'
 const CardList = (props) => {
   const { beers } = props;
 
-  const getBeerCardsJsx = (beer) => {
+  const getBeerCardsJsx = (beer) => (
     <div className={styles.card} key={beer.id}>
       <Card beer={beer} />
     </div>
-  }
+  )
 
   return <section className={styles.cards}> {beers.map(getBeerCardsJsx)} </section>
 }
