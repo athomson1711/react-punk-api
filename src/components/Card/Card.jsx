59929d7666f4152b51d243070fb0b81a9b62cc65
Andrew Thomson <athomson1711@gmail.com>
2022-07-21 15:46:36 +0100

    Card interior elements created

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,6 +5,10 @@ const Card = () => {
   return (
     <div className={styles.card}>
         <img className={styles.img}></img>
+        <section>
+            <h1>Beer Name</h1>
+            <p>Beer info...</p>
+        </section>
     </div>
   )
 }
