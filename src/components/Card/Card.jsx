92dbac659fedd6841e517bb9ddc60c74d99980ef
Andrew Thomson <athomson1711@gmail.com>
2022-07-21 15:43:10 +0100

    Card component created

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Card.module.scss'
+
+const Card = () => {
+  return (
+    <div className={styles.card}>
+        <img className={styles.img}></img>
+    </div>
+  )
+}
+
+export default Card
--- src/components/CardArea/CardArea.jsx
+++ src/components/CardArea/CardArea.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './CardArea.module.scss'
+import Card from '../Card/Card'
 
 const CardArea = () => {
   return (
     <div className={styles.cardArea}>
-
+        <Card />
     </div>
   )
 }
