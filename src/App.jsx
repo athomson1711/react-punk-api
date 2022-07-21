f92e30723a51d1a4250d7a180e21af65790f0d22
Andrew Thomson <athomson1711@gmail.com>
2022-07-21 16:17:37 +0100

    API added and getting array of beer objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import styles from "./App.module.scss";
+import { useState, useEffect } from 'react'
 
 import NavBar from "./components/NavBar";
 import CardArea from "./components/CardArea";
 
+// import getBeers from "./services/beer.service";
+
 const App = () => {
+
+  const API_URL = "https://api.punkapi.com/v2/beers"
+
+  // const getBeers = (searchTerm) => {
+  //     return fetch(`${API_URL}?beer_name=${searchTerm}`)
+  //     .then((res) => res.json())
+  //     .then((jsonResponse) =>{
+  //         return jsonResponse.beers.map((beer) => mapBeers(beer));
+  //     })
+  // }
+
+
+  const getBeers = () => {
+      return fetch(`${API_URL}`)
+      .then((res) => res.json())
+      .then((jsonResponse) =>{
+          return (jsonResponse);
+      })
+  }
+  
+  console.log(getBeers())
+
   return (
     <>
     <main className={styles.main}>
     <NavBar />
-    <CardArea />
+    <CardArea beers={getBeers}/>
+
     
     </main>
     </>      
