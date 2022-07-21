f7597ec73609ba336a15dc90c63a1dae405861c5
Andrew Thomson <athomson1711@gmail.com>
2022-07-21 15:33:44 +0100

    main area created

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import NavBar from "./components/NavBar";
 const App = () => {
   return (
     <>
+    <main className={styles.main}>
     <NavBar />
+    <div className={styles.cardArea}>
+
+    </div>
+    </main>
     </>  
   );
 }
