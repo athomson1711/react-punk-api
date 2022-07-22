22f2fccd5aa5d8e98ecb4adf632782cb6c30b14c
Andrew Thomson <athomson1711@gmail.com>
2022-07-22 09:34:09 +0100

    overflow scroll added to cards & CardList

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,6 @@ import CardArea from "./components/CardArea";
 
 const App = () => {
 
-  // const beerData = [ 
-  //   {
-  //   name: "Beer 1",
-  //   tagline: "Beer 1 is good",
-  //   abv: 1,
-  //   description: "Beer 1 is a good beer says person 1"
-  //   },
-    
-  //   {
-  //   name: "Beer 2",
-  //   tagline: "Beer 2 is good",
-  //   abv: 2,
-  //   description: "Beer 2 is a good beer says person 2"
-  //   },
-    
-  //   {
-  //   name: "Beer 3",
-  //   tagline: "Beer 3 is good",
-  //   abv: 3,
-  //   description: "Beer 3 is a good beer says person 3"
-  //   },
-    
-  //   ]
-
 const [beers, setBeers] = useState([])
 
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import styles from './Card.module.scss'
 
 const Card = (props) => {
-const { name, tagline, abv, description } = props.beer;
+const { name, tagline, abv, description, image_url } = props.beer;
 
 
   return (
     <div className={styles.card}>
-        <img className={styles.img} alt={name}></img>
-        <section>
+        <img className={styles.img} alt={name} src={image_url}></img>
+        <section className={styles.beerInfo}>
             <h1>{name}</h1>
             <h4>{tagline}</h4>
             <p>ABV: {abv}%</p>
-            <p>{description}</p>
+            <div className={styles.descriptionContainer}>
+              <p className={styles.description}>{description}</p>
+            </div>
         </section>
     </div>
   )
-}
+};
 
 export default Card
