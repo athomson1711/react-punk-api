62e82a7f1eee4768ebd66905989c03096c0fe04a
Andrew Thomson <athomson1711@gmail.com>
2022-07-23 15:56:32 +0100

    altered card layout

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import styles from './Card.module.scss'
 
 const Card = (props) => {
-const { name, tagline, abv, description, image_url, ph, first_brewed} = props.beer;
+  const { name, tagline, abv, description, image_url, ph, first_brewed } = props.beer;
 
 
   return (
     <div className={styles.card}>
-        <img className={styles.img} alt={name} src={image_url}></img>
-        <section className={styles.beerInfo}>
-            <h1>{name}</h1>
-            <h4>{tagline}</h4>
-            <p>ABV: {abv}%</p>
-            <p>PH: {ph}</p>
-            <p>First brewed: {first_brewed}</p>
-            <div className={styles.descriptionContainer}>
+      <img className={styles.img} alt={name} src={image_url}></img>
+      <section className={styles.beerInfo}>
+        <h1>{name}</h1>
+        {/* <h4>{tagline}</h4> */}
+        <div>
+          <p>ABV: {abv}%</p>
+          <p>PH: {ph}</p>
+        </div>
+        <p>First brewed: {first_brewed}</p>
+        {/* <div className={styles.descriptionContainer}>
               <p className={styles.description}>{description}</p>
-            </div>
-        </section>
+            </div> */}
+      </section>
     </div>
   )
 };
